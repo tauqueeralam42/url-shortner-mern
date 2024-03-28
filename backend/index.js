f2230812ab0d5a2215497d38afb356b7258b9b32
Tauqueer Alam <tauqueeralam42@gmail.com>
2024-03-28 23:15:02 +0530

    routes added

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const {connectToMongoDB} = require('./connect');
+
+const urlRoute = require("./routes/url");
 
 const app = express();
 const PORT = 8001;
+
+connectToMongoDB("mongodb://localhost:27017/short-url")
+.then( () => console.log("MongoDB is connected"));
+
+app.use("/url", urlRoute);
 
 app.listen(PORT, () => {
     console.log(`Server started at port no. ${PORT}`);
